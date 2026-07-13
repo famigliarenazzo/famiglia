c155675199af70aba287b7670250ce420a5d3d51
famigliarenazzo <6hb5vhdfvs@privaterelay.appleid.com>
2026-07-13 21:49:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker del portale di famiglia.
    Mette in cache solo l'involucro dell'app (HTML, CSS, icone).
    I dati passano sempre dalla rete: mai in cache. */
-var VERSION = "famiglia-v3";
+var VERSION = "famiglia-v4";
 var SHELL = [
   "./",
   "./index.html",
